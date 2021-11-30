3d05ec44f51e58d92d186c637f51eb4672c354dc
Weizhe (Wesley) Chen <chenweiz@iu.edu>
2021-11-30 12:15:19 -0500

    Added differential_driver_node.

--- src/nodes/differential_driver_node.cpp
+++ src/nodes/differential_driver_node.cpp
@@ -1,5 +1,12 @@
+#include <ros/ros.h>
+
 int main(int argc, char** argv)
 {
-  //
+  ros::init(argc, argv, "differential_driver");
+  ros::NodeHandle nh;
+
+  // Construct an object here
+
+  ros::spin();
   return 0;
 }
